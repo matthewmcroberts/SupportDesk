fdfa64707beebb5f0cf13fd85812d8564ed7e061
Matthew <mm20captingoof@gmail.com>
2026-09-23 19:01:55 -0400

    refactor: name

--- src/main/java/com/matthewmcroberts/booklibrary2/SupportDeskApplication.java
+++ src/main/java/com/matthewmcroberts/booklibrary2/SupportDeskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookLibrary2Application {
+public class SupportDeskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookLibrary2Application.class, args);
+        SpringApplication.run(SupportDeskApplication.class, args);
     }
 
 }
